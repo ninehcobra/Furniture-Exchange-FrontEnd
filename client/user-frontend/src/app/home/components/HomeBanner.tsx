86c81352a151721177d351eb41da44707eda1b17
chinhtruong2903 <chinh.truong@qkit.vn>
2024-09-29 15:51:42 +0700

    feat: add home banner

---
+++ client/user-frontend/src/app/home/components/HomeBanner.tsx
@@ -0,0 +1,66 @@
+/* eslint-disable @next/next/no-img-element */
+'use client'
+
+import { Carousel } from 'antd'
+import './home-banner.scss'
+
+export default function HomeBanner(): React.ReactNode {
+  const onChange = (currentSlide: number): void => {
+    console.log(currentSlide)
+  }
+
+  const bannerButtons = [
+    { src: './images/btn-background-image/btn-1.png', title: 'Voucher giảm giá 10%' },
+    { src: './images/btn-background-image/btn-2.png', title: 'Miễn phí ship' },
+    { src: './images/btn-background-image/btn-3.png', title: 'Săn sale' },
+    { src: './images/btn-background-image/btn-4.png', title: 'Mã giảm giá' },
+    { src: './images/btn-background-image/btn-5.png', title: 'ESOLD siêu rẻ' },
+    { src: './images/btn-background-image/btn-6.png', title: 'Voucher giảm giá 30%' },
+    { src: './images/btn-background-image/btn-7.png', title: 'Hàng quốc tế' },
+    { src: './images/btn-background-image/btn-8.png', title: 'Dịch vụ khác' }
+  ]
+
+  return (
+    <div className='home-banner'>
+      <div className='container pt-4'>
+        <div className='d-flex' style={{ maxHeight: '240px' }}>
+          <Carousel autoplay style={{ width: '800px', borderRadius: '4px' }} afterChange={onChange}>
+            <div>
+              <img src='./images/banner/banner-1.jpg' className='h-100 w-100' alt='anh banner' />
+            </div>
+            <div>
+              <img src='./images/banner/banner-2.jpg' className='h-100 w-100' alt='anh banner' />
+            </div>
+            <div>
+              <img src='./images/banner/banner-3.jpg' className='h-100 w-100' alt='anh banner' />
+            </div>
+          </Carousel>
+          <div className='ps-1'>
+            <img
+              src='./images/banner/banner-4.jpg'
+              style={{ height: '120px', borderRadius: '4px' }}
+              className=' w-100 pb-1'
+              alt='anh banner'
+            />
+            <img
+              src='./images/banner/banner-5.jpg'
+              style={{ height: '120px', borderRadius: '4px' }}
+              className=' w-100 '
+              alt='anh banner'
+            />
+          </div>
+        </div>
+        <div className='w-100 mt-3 mb-2 row row-cols-8 g-3'>
+          {bannerButtons.map((button, index) => (
+            <div key={index} className='col'>
+              <div className='d-flex flex-column align-items-center'>
+                <img alt='banner-btn' className='banner-btn ' src={button.src} />
+                <p className='text-center body-xs mt-1'>{button.title}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
--- client/user-frontend/src/app/home/page.tsx
+++ client/user-frontend/src/app/home/page.tsx
@@ -1,12 +1,15 @@
 import Header from '@/common/components/Header'
 import './home.scss'
 import Footer from '@/common/components/Footer'
+import HomeBanner from './components/HomeBanner'
 
 export default function Home(): React.ReactNode {
   return (
-    <div>
+    <div className='home-page'>
       <Header />
-      <div className='content-wrapper'></div>
+      <div className='content-wrapper'>
+        <HomeBanner />
+      </div>
       <Footer />
     </div>
   )
