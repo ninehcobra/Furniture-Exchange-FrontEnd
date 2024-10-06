3186c0f6b2ce89e866301a5a21a49e1ae9a9e3d8
chinhtruong2903 <chinh.truong@qkit.vn>
2024-10-06 22:14:29 +0700

    wip

--- client/user-frontend/src/common/components/GoogleAdUnitClient.tsx
+++ client/user-frontend/src/common/components/GoogleAdUnitClient.tsx
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-redundant-type-constituents */
 'use client'
 import React from 'react'
 import { usePathname, useSearchParams } from 'next/navigation'
@@ -17,7 +21,7 @@ const GoogleAdUnitClient: React.FC<GoogleAdUnitProps> = ({ children }) => {
   const searchParams = useSearchParams()
   React.useEffect(() => {
     try {
-      ;(window.adsbygoogle = window.adsbygoogle || []).push({})
+      (window.adsbygoogle = window.adsbygoogle || []).push({})
     } catch (err) {
       console.error(err)
     }
