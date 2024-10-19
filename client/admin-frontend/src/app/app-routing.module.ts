38b0cd8684494aebf03b05c48cc06bd40cb6568c
chinhtruong2903 <chinh.truong@qkit.vn>
2024-10-19 22:36:05 +0700

    fix: fix flow default redirect

--- client/admin-frontend/src/app/app-routing.module.ts
+++ client/admin-frontend/src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
     children: [
       {
         path: '',
-        redirectTo: '/dashboards/dashboard1',
+        redirectTo: '/authentication/side-login',
         pathMatch: 'full',
       },
       {
--- client/admin-frontend/src/environments/environment.ts
+++ client/admin-frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  API_URL: 'https://13.215.203.58/api/v1',
-  SOCKET_URL: 'http://13.215.203.58:3000',
+  API_URL: 'https://54.169.214.177/api/v1',
+  SOCKET_URL: 'http://54.169.214.177:3000/',
 };
